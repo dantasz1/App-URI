76b7208384557e17b761e99028aa5700250f8b6c
Andre <dantasluizandre@gmail.com>
2026-02-26 16:26:20 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/fatecpg/myapplication/MainActivity.kt
@@ -0,0 +1,55 @@
+package br.edu.fatecpg.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtU = findViewById<EditText>(R.id.edtTensao)
+        val edtR = findViewById<EditText>(R.id.edtResistencia)
+        val edtI = findViewById<EditText>(R.id.edtCorrente)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+
+        btnCalcular.setOnClickListener {
+
+            val u = edtU.text.toString()
+            val r = edtR.text.toString()
+            val i = edtI.text.toString()
+
+            // Verifica se pelo menos 2 campos estão preenchidos
+            val preenchidos = listOf(u, r, i).count { it.isNotEmpty() }
+            if (preenchidos < 2) {
+                Toast.makeText(this, "Preencha pelo menos 2 campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (preenchidos > 2) {
+                Toast.makeText(this, "Deixe o campo que deseja calcular vazio", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            try {
+                val resultado = when {
+                    u.isEmpty() -> r.toDouble() * i.toDouble()
+                    r.isEmpty() -> u.toDouble() / i.toDouble()
+                    i.isEmpty() -> u.toDouble() / r.toDouble()
+                    else -> 0.0
+                }
+
+                txtResultado.text = "Resultado: %.2f".format(resultado)
+
+            } catch (e: Exception) {
+                Toast.makeText(this, "Digite apenas números válidos e positivos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
